9b39fd2a58205ed6f5688fb007a2f43b458d8b63
Sam Lanning <sam@samlanning.com>
2019-03-11 19:25:52 -0700

    Display error badge when missing permissions

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,75 +1,84 @@
 import { LinkConfiguration, getConfig, addListener } from '../shared/config';
 import { missingOriginPermissions } from '../shared/permissions';
 
-/**
- * Maintained list of origins that we have permission for
- */
-const grantedOrigins = new Set<string>();
+getConfig().then(config => {
 
-/**
- * Update the value of grantedOrigins based on the current list of permissions
- */
-function checkPermissions() {
-  console.log('checking permissions');
-  chrome.permissions.getAll(permissions => {
-    grantedOrigins.clear();
-    if (permissions.origins) {
-      for (const origin of permissions.origins)
-        grantedOrigins.add(origin);
+  addListener(c => config = c);
+
+  /**
+   * Maintained list of origins that we have permission for
+   */
+  const grantedOrigins = new Set<string>();
+
+  /**
+   * Update the value of grantedOrigins based on the current list of permissions
+   */
+  function checkPermissions() {
+    console.log('checking permissions');
+    chrome.permissions.getAll(permissions => {
+      grantedOrigins.clear();
+      if (permissions.origins) {
+        for (const origin of permissions.origins)
+          grantedOrigins.add(origin);
+      }
+      const missingOrigins = missingOriginPermissions(config, grantedOrigins);
+      if (missingOrigins.length > 0) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: [247, 108, 108, 255] });
+        chrome.browserAction.setBadgeText({ text: 'ERR' });
+        chrome.browserAction.setTitle({ title: 'Missing Permissions' });
+      } else {
+        chrome.browserAction.setBadgeText({ text: '' });
+        chrome.browserAction.setTitle({ title: 'Configure Links for GitHub & Jira' });
+      }
+      console.log('permissions: ', permissions, grantedOrigins);
+    });
+  }
+
+  // Check permissions on startup, and when changes.
+  checkPermissions();
+  chrome.permissions.onAdded.addListener(checkPermissions);
+  chrome.permissions.onRemoved.addListener(checkPermissions);
+
+  /**
+   * TODO: create a warning icon when permissions are not correct
+   */
+  chrome.browserAction.onClicked.addListener(tab => {
+    chrome.runtime.openOptionsPage();
+    // TODO: move the following to the options page
+    const missingOrigins = missingOriginPermissions(config, grantedOrigins);
+    if (missingOrigins.length > 0) {
+      chrome.permissions.request({ origins: missingOrigins });
     }
-    console.log('permissions: ', permissions, grantedOrigins);
   });
-}
 
-// Check permissions on startup, and when changes.
-checkPermissions();
-chrome.permissions.onAdded.addListener(checkPermissions);
-chrome.permissions.onRemoved.addListener(checkPermissions);
+  /**
+   * Open the options page when the plugin is first installed
+   */
+  chrome.runtime.onInstalled.addListener(function (object) {
+    chrome.runtime.openOptionsPage();
+  });
 
-let config: LinkConfiguration[] = [];
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+      intectContentScripts(tab);
+    }
+  });
 
-addListener(c => config = c);
-getConfig().then(c => config = c);
+  chrome.tabs.query({}, tabs => {
+    tabs.map(intectContentScripts);
+  });
 
-/**
- * TODO: create a warning icon when permissions are not correct
- */
-chrome.browserAction.onClicked.addListener(tab => {
-  chrome.runtime.openOptionsPage();
-  // TODO: move the following to the options page
-  const missingOrigins = missingOriginPermissions(config, grantedOrigins);
-  console.log('Missing Origins:', missingOrigins);
-  if (missingOrigins.length > 0) {
-    chrome.permissions.request({origins: missingOrigins});
-  }
-});
-
-/**
- * Open the options page when the plugin is first installed
- */
-chrome.runtime.onInstalled.addListener(function (object) {
-  chrome.runtime.openOptionsPage();
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    intectContentScripts(tab);
-  }
-});
-
-chrome.tabs.query({}, tabs => {
-  tabs.map(intectContentScripts);
-});
-
-function intectContentScripts(tab: chrome.tabs.Tab) {
-  // Check if we can read the URL
-  if (!tab.url || !tab.id) return;
-  const url = new URL(tab.url);
-  for (const c of config) {
-    if (url.host === c.gitHubDomain) {
-      chrome.tabs.executeScript(tab.id, {
-        file: 'content-scripts/github.js'
-      });
+  function intectContentScripts(tab: chrome.tabs.Tab) {
+    // Check if we can read the URL
+    if (!tab.url || !tab.id) return;
+    const url = new URL(tab.url);
+    for (const c of config) {
+      if (url.host === c.gitHubDomain) {
+        chrome.tabs.executeScript(tab.id, {
+          file: 'content-scripts/github.js'
+        });
+      }
     }
   }
-}
+});
+
